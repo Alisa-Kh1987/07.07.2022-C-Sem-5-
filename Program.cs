1466d713c2ef70959b271dc9b27a78f3cef2f2cb
Алиса <petrunina.alisa@gmail.com>
2022-07-07 20:51:48 +0300

    примеры форматирования

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Примеры форматирования
+
+//string name = "Имя";
+//int age = 99;
+//Console.WriteLine("Имя: {0}  Возраст: {1}", name, age);
+//
+//int number = 23;
+//string result = string.Format("{0:f}", number);
+//
+//Console.WriteLine(result);
+
+//double number2 = 45.08;
+//string result2 = string.Format("{0:f5}", number2);
+//Console.WriteLine(number2);
+//Console.WriteLine(result2);
+//
+//string name = "Имя";
+//int age = 99;
+//Console.WriteLine($"Имя: {name}  Возраст: {age}");
+//int x = 7;
+//int y = 8;
+//string result = $"{x} + {y} = {x + y}";
+//Console.WriteLine(result);
+//
+//string name = "Елена";
+//int age = 99;
+//
+//Console.WriteLine($"Имя: {name, -10} Возраст: {age}"); // пробелы после
+//Console.WriteLin($"Имя: {name, 15} Возраст: {age}"); // пробелы до
+
